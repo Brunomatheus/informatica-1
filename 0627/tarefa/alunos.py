17b4e69f1245f52cec05a00d637831fb3b178b2c
Bruno Matheus <brunomatheus17@yahoo.com>
2026-06-28 11:09:31 -0300

    corrigindo bugs

--- 0627/tarefa/alunos.py
+++ 0627/tarefa/alunos.py
@@ -5,7 +5,7 @@ def cadastrar_aluno(aluno):
     nota1 = int(input("Digite a sua primeira nota: "))
     nota2 = int(input("Digite a sua segunda nota: "))
     media = 0
-
+    media = (nota1 + nota2)/2
     listar = {
         "nome":nome,
         "turma":turma,
@@ -15,25 +15,40 @@ def cadastrar_aluno(aluno):
     }
     aluno.append(listar)
 def media_aluno(aluno):
+    if(len(aluno) == 0):
+        print("Nenhum aluno ainda cadastrado.")
+        return
     perguntar = input("Digite o nome do aluno que deseja ver a media: ")
     media = 0
     for alunos in aluno:
         if(perguntar.lower() == alunos["nome"].lower()):
             media = (alunos["nota1"] + alunos["nota2"])/2
             alunos["media"] = media
-    print(f"A media do aluno é {media}")
-
+            print(f"A media do aluno é {media}")
+            return
+    else:
+        print("Nome de usuario não encontrado!")    
+        
 def listar_aluno(aluno):
+    if(len(aluno) == 0):
+        print("Nenhum aluno ainda cadastrado.")
+        return
     print("--- Listando alunos ---")
     for alunos in aluno:
         print(f"Aluno(a): {alunos["nome"]}")
         print(f"Turma: {alunos["turma"]}")
         print(f"Media: {alunos["media"]}")
+
 def aprovados(aluno):
+    if(len(aluno) == 0):
+        print("Nenhum aluno ainda cadastrado.")
+        return
     for alunos in aluno:
         if(alunos["media"] >= 7 ):
              print(f"Aluno(a): {alunos["nome"]} aprovado")
-             return
+             
+        else:
+            print("Nenhum aluno foi aprovado!")
 while(True):
     print("--- Menu ---")
     print("(1) - Cadastrar aluno")
